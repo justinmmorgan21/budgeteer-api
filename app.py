032ba33f44233d252fee921db786f4086ba1c208
justinmmorgan21 <justinmorgan21@gmail.com>
2025-04-22 12:04:18 -0500

    added categories create action

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.errorhandler(Exception)
 def handle_exception(e):
-    status_code = getattr(e, 'code', 500)
-    return jsonify(error=str(e)), status_code
+  status_code = getattr(e, 'code', 500)
+  return jsonify(error=str(e)), status_code
 
 @app.route('/transactions/upload', methods=['POST'])
 def transactions_create():
   file = request.files['file']
   if not file:
-      return jsonify(error="No file uploded"), 400
+    return jsonify(error="No file uploded"), 400
   file_path = f"./uploads/{file.filename}"
   file.save(file_path)
 
@@ -25,12 +25,12 @@ def transactions_create():
   try:
     transactions = Transaction.read_statement(file_path)
     for t in transactions:
-        session.add(t)
+      session.add(t)
     session.commit()
     return jsonify({"message": f"{len(transactions)} transactions added."})
   except Exception as e:
-        session.rollback()
-        raise e
+    session.rollback()
+    raise e
   finally:
     session.close()
 
@@ -40,63 +40,79 @@ def transaction_index():
   try:
     transactions = session.scalars(select(Transaction)).all()
     if not transactions:
-            raise NotFound(f"Transactions not found.")
+      raise NotFound(f"Transactions not found.")
     return jsonify([t.to_dict() for t in transactions])
   except Exception as e:
-        raise e
+      raise e
   finally:
     session.close()
 
 @app.route('/transactions/<int:id>')
 def transaction_show(id):
-   session = SessionLocal()
-   try:
+  session = SessionLocal()
+  try:
     transaction = session.scalar(select(Transaction).where(Transaction.id==id))
     if not transaction:
-            raise NotFound(f"Transaction with id {id} not found.")
+      raise NotFound(f"Transaction with id {id} not found.")
     return jsonify(transaction.to_dict())
-   except Exception as e:
-        raise e
-   finally:
+  except Exception as e:
+    raise e
+  finally:
     session.close()
 
 @app.route('/transactions/<int:id>', methods=['PATCH'])
 def transaction_update(id):
-   session = SessionLocal()
-   try:
+  session = SessionLocal()
+  try:
     transaction = session.scalar(select(Transaction).where(Transaction.id==id))
     if not transaction:
-            raise NotFound(f"Transaction with id {id} not found.")
+      raise NotFound(f"Transaction with id {id} not found.")
     transaction.category_id = int(category_id) if (category_id := request.form.get("category")) else transaction.category_id
     transaction.tag_id = int(tag_id) if (tag_id := request.form.get("tag")) else transaction.tag_id
     session.commit()
     return jsonify(transaction.to_dict())
-   except Exception as e:
-        session.rollback()
-        raise e
-   finally:
+  except Exception as e:
+    session.rollback()
+    raise e
+  finally:
     session.close()
 
 @app.route('/categories')
 def category_index():
-   session = SessionLocal()
-   try:
+  session = SessionLocal()
+  try:
     categories = session.scalars(select(Category)).all()
     return jsonify([c.to_dict(True) for c in categories])
-   except Exception as e:
-        raise e
-   finally:
+  except Exception as e:
+    raise e
+  finally:
     session.close()
+
+@app.route('/categories', methods=['POST'])
+def category_create():
+  session = SessionLocal()
+  try:
+    name = request.form.get('name')
+    category = Category(name=name)
+    session.add(category)
+    session.commit()
+    return jsonify(category.to_dict())
+  except Exception as e:
+    session.rollback()
+    raise e
+  finally:
+    session.close()
+    
 
 @app.route('/tags')
 def tag_index():
-   session = SessionLocal()
-   try:
+  session = SessionLocal()
+  try:
     tags = session.scalars(select(Tag)).all()
     return jsonify([t.to_dict() for t in tags])
-   except Exception as e:
-        raise e
-   finally:
+  except Exception as e:
+    raise e
+  finally:
     session.close()
 
 if __name__ == "__main__":
